32a267e1be8f1838878ddc9875789b67867ce20a
basopro1511 <nguyenquochoang2003ct@gmail.com>
2024-09-03 11:20:40 +0700

    add all web

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BellisoProject.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
